e378ba0bedb16d0bff2b678c07710b5b0ec60675
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-13 18:38:09 +0700

    first commit

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+public:
+mahasiswa();
+};
+
+mahasiswa::mahasiswa(){
+    cout << "constructor terpanggil"<<endl;
+};
+
+int main(){
+    mahasiswa mhs;
+    return 0;
+}
